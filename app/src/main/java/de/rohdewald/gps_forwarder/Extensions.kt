c425fb0a3f63290264f73a5ec509f2fb8495ce13
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2018-10-28 13:47:06 +0100

    GPS: show all times only if there is a problem, and now as Error

--- app/src/main/java/de/rohdewald/gps_forwarder/Extensions.kt
+++ app/src/main/java/de/rohdewald/gps_forwarder/Extensions.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import java.text.SimpleDateFormat
 import java.util.Date
+import kotlin.math.abs
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
@@ -22,11 +23,18 @@ fun Date.toLogString() = timeFormat.format(this)
 fun Location.toLogString(): String {
     val fmt = SimpleDateFormat("YYYY dd.MM HH:mm:ss.SSS")
     val bootTime = java.lang.System.currentTimeMillis() - android.os.SystemClock.elapsedRealtime()
-    val sysTime = Date()
+    val sysTime = System.currentTimeMillis()
     val gpsEtToTime = bootTime + elapsedRealtimeNanos / 1000000L
     val altct = if (altitudeAsCounter) " ${altitude.toInt()}" else ""
-    return "Systime ${fmt.format(sysTime)} location.time ${fmt.format(time)} bootTime ${fmt.format(bootTime)} gpsEtToTime ${fmt.format(gpsEtToTime)} ${"%.6f".format(latitude)} ${"%.6f".format(longitude)}$altct"
+    val times = mutableListOf(sysTime, gpsEtToTime, time)
+    if (abs(times.min()!! - times.max()!!) > 60000L) {
+        // more than 60s
+        logError("GPS Time problem: Systime ${fmt.format(sysTime)} location.time ${fmt.format(time)} bootTime ${fmt.format(bootTime)} gpsEtToTime ${fmt.format(gpsEtToTime)}")
+    }
+    return "${"%.6f".format(latitude)} ${"%.6f".format(longitude)}$altct"
 }
+
+
 
 fun SharedPreferences.putString(key: String, value:String) =
         edit()?.apply {
